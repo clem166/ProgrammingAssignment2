49770088e40c2075579b7cbd59d1fabb30adb844
Clement Y <aka_clement166@hotmail.com>
2018-09-23 22:10:30 +1000

    Assignment Edit
    Clement edit of R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions create a list of matrices and their inputs
+## If matrix has been inversed before, it will grab the data instead of recalculating
+## Establishing list of functions and matrix containing cached data
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix = function (c = matrix()) {
+  s <- NULL
+  set <- function (d = matrix()) {
+    c <<- d
+    s <<- NULL
+  }
+  get <- function () c
+  setinverse <- function(s1) s <<- s1
+  getinverse <- function() s
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
+## Return a matrix that is the inverse of 'x'
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve = function(c, ...) {
+  s = c$getinverse()
+  if (!is.null(s)) {
+    message("getting cached data")
+    # print(is.null(s))
+    #    print(s)
+    return(s)
+  }
+  data= c$get()
+  #  print(data)
+  s = solve(data, ...)
+  c$setinverse(s)
+  s
 }
+
